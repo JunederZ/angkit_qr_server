b6b5b8b9cd69e445967e7b7b05dd27c2eba31764
JunederZ <arjunadimas000@gmail.com>
2023-12-21 16:21:53 +0700

    fix login

--- routes/login.py
+++ routes/login.py
@@ -14,12 +14,17 @@ def login():
     user = Users.select().where(Users.username == username)
     userObject = user.first()
     site_id = ''
+    if not user.exists():
+        return make_response({
+            'status': 'error',
+            'message': 'not found'
+        }, 404)
     if userObject.role == 'Peternakan':
         site_id = Peternakan.select().where(Peternakan.user == userObject.username).first().id
     elif userObject.role == 'Distributor':
         site_id = Distributor.select().where(Distributor.user == userObject.username).first().id
     try:
-        if user.exists() and PasswordHasher().verify(password=password, hash=userObject.password):
+        if PasswordHasher().verify(password=password, hash=userObject.password):
             return make_response({
                 'status': 'ok',
                 'user': userObject.username,
