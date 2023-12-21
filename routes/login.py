33daee311bb8986658422add0cca22bfaa70dea7
Kaisan Tsabit <ktsabit@gmail.com>
2023-12-21 13:19:23 +0700

    login response with backrefs

--- routes/login.py
+++ routes/login.py
@@ -1,6 +1,8 @@
 from flask import request, make_response
 from argon2 import PasswordHasher
 import argon2
+from playhouse.shortcuts import model_to_dict
+
 from database.models import *
 from flasgger import swag_from
 
@@ -12,12 +14,12 @@ def login():
 
     user = Users.select().where(Users.username == username)
     userObject = user.first()
+    user_res = model_to_dict(userObject, backrefs=True)
     try:
         if user.exists() and PasswordHasher().verify(password=password, hash=userObject.password):
             return make_response({
                 'status': 'ok',
-                'username': userObject.username,
-                'role': userObject.role,
+                'user': user_res,
             }, 200)
     except argon2.exceptions.VerifyMismatchError:
         return make_response({
